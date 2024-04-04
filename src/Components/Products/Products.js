81c959a287ea9628e33021fe8f4abe5c0cc5edf2
tadikamalla23 <kavya.tadik@gmail.com>
2024-04-04 15:04:09 -0400

    Created Products page

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Products.css'
+
+class Products extends React.Component {
+  render() { 
+    return ( 
+          <><br/>
+            <div className="row container-fluid">
+            {this.props.products.map((product) => (
+              <div key={product.pid} className="col-xl-3" id="products">
+                <div className="card h-100">
+                  <img
+                    src={product.image}
+                    className="card-img-top w-100 h-75"
+                    alt="Product"
+                  />
+                  <div className="card-body">
+                    <Link className="btn btn-primary stretched-link" >
+                      Shop Now
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
+            </div>
+          </> );
+  }
+}
+ 
+export default Products;
