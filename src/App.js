89c22737323df02607cdb3a6d6654a21b8a8f2f4
raisusmita <susmitathulung53@gmail.com>
2024-04-04 15:41:12 -0400

    navbar and header section created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      FurnituRistic...
+      <Navbar /> <br />
+      <Header />
+
     </div>
   );
 }
